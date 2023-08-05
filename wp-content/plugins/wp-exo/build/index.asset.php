196cb7bb73d167e7a198cbe2f8c1f7b39d3ccc7e
refined-web <ademceylan67200@gmail.com>
2023-08-05 13:47:25 +0200

    5 aout

---
+++ wp-content/plugins/wp-exo/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c662b88b13e8dfa016fd');
---
+++ wp-content/plugins/wp-exo/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '3d1f78e87cbbb7391515');
